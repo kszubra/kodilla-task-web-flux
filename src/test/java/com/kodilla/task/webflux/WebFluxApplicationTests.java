5e0a951088f5ecf0372a31b42f87a4e2e68d4ee7
Kacper Szubra <kacper.szubra@nvt.pl>
2021-10-19 14:24:12 +0200

    iknitial copmmit

---
+++ src/test/java/com/kodilla/task/webflux/WebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.task.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
